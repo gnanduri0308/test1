ad47bdc404056fac367eb0927f763e27ae813b9e
jchaladigit <57153708+jchaladigit@users.noreply.github.com>
2020-04-05 15:48:32 -0400

    committing the code

---
+++ src/test/java/org/zerocode/example/two/helloTest.java
@@ -0,0 +1,16 @@
+package org.zerocode.example.two;
+
+import org.jsmart.zerocode.core.domain.Scenario;
+import org.jsmart.zerocode.core.runner.ZeroCodeUnitRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(ZeroCodeUnitRunner.class)
+
+
+public class helloTest {
+    @Test
+    @Scenario("hello.json")
+    public void testGet() throws Exception {
+    }
+}
